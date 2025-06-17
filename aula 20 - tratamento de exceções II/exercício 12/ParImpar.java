8390ab51013ac84c17f77b7a5f8124f92d8e24dc
milena <milenaabernardi@gmail.com>
2025-06-16 23:22:07 -0300

    organização

---
+++ aula 20 - tratamento de exceções II/exercício 12/ParImpar.java
@@ -0,0 +1,15 @@
+public class ParImpar {
+    public static void main(String args[]) {
+        try {
+            int num = Integer.parseInt(args[0]);
+            if ((num % 2) == 0)
+                System.out.println("Número par");
+            else
+                System.out.println("Número impar");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Não foi fornecido um argumento.");
+        } catch (NumberFormatException e) {
+            System.out.println("Argumento inválido. Digite um número inteiro.");
+        }
+    }
+}
